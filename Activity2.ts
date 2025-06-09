2b7e7d31ba9b1f8dd3978cfb4296381e555e1138
PodaMounitha <176000994+PodaMounitha@users.noreply.github.com>
2025-06-09 10:14:56 +0530

    Add files via upload

---
+++ Activity2.ts
@@ -0,0 +1,23 @@
+/*
+Create a variable for your favorite fruit and print it.
+Write a function that takes a number and prints double its value.
+
+Add a single-line and a multi-line comment to your code.
+
+Define a class called Person with a method sayHello that prints a greeting.
+*/
+
+let fruit :  string = "Mange";
+console.log("My favourite fruit is : " + fruit);
+
+// Single-line comment
+
+/*
+This is a multi-line comment.
+*/
+
+function doublenumber(num: number) : void{
+    console.log("Double of " +num+ " is "+ num*2);
+}
+
+doublenumber(5);
