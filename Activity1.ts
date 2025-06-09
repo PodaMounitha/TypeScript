d759884bd70eac84521c731c26bed04dead02351
PodaMounitha <176000994+PodaMounitha@users.noreply.github.com>
2025-06-09 10:07:26 +0530

    Add files via upload

---
+++ Activity1.ts
@@ -0,0 +1,17 @@
+/*
+Change the message variable to your own name and print a personalized greetinng.
+Try declaring a variable for your age and print it with a message.
+What happens if you try to assign a number to a variable declared as a string?
+*/
+
+let message : string = "Hey There, How are you?";
+message = "Hello, Poda Mounitha";
+console.log(message);
+
+let age : number = 19;
+console.log("My age is :" + age);
+
+// If I try to assign a number to a variable declared as a string, it will throw an error.
+
+let Myname :  string = "Poda Mounitha";
+// Myname = 19; // This will cause a TypeScript error: Type 'number' is not assignable to type 'string'.
